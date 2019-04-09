4f1f6f2be20d71551d521c0add4b7675600b07aa
Ivo Angelov <jagdcake@tuta.io>
2019-04-09 18:29:35 +0300

    Create function to add new director inputs

---
+++ public/js/form.js
@@ -0,0 +1,22 @@
+const addDirectorButton = document.querySelector('.add-director-field');
+
+function addNewInput(addButton) {
+    addButton.addEventListener('click', function() {
+        const formSection = this.parentElement;
+        const formField = formSection.querySelector('input');
+
+        let index = formSection.querySelectorAll('input').length;
+        const newIndex = index++;
+
+        const input = formField.cloneNode(false);
+        const inputName = input.getAttribute('name');
+        const inputId = input.getAttribute('id');
+
+        input.setAttribute('name', inputName.replace('0', newIndex));
+        input.setAttribute('id', inputId.replace('0', newIndex));
+        formSection.insertAdjacentElement('beforeend',input);
+        input.value = '';
+    });
+}
+
+addNewInput(addDirectorButton);
