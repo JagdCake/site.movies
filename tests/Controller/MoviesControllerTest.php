06023a12d71b9f6b6e96d66833de618c324dd72a
Ivo Angelov <jagdcake@tuta.io>
2019-04-17 13:09:35 +0300

    Test if the index route redirect is permanent

--- tests/Controller/MoviesControllerTest.php
+++ tests/Controller/MoviesControllerTest.php
@@ -5,4 +5,16 @@ namespace App\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MoviesControllerTest extends WebTestCase {
+
+    public function testRootRedirect() {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+        $response = $client->getResponse();
+
+        $this->assertEquals(
+            301,
+            $response->getStatusCode()
+        );
+    }
 }
