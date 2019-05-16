ddbec01ade2dad69108353b23d7f33b6a5d86f1a
Ivo Angelov <jagdcake@tuta.io>
2019-05-16 18:49:53 +0300

    Create function to purify movie list titles

---
+++ public/js/movie-search.ts
@@ -0,0 +1,8 @@
+const movieTitle = (movieData: string): string => {
+    let title: string;
+    title = movieData.replace(/(\|\s\d{4})/g, ''); // removes the separator (|) and the year
+    title = title.trim(); // could pose a problem
+    title = title.toLowerCase();
+
+    return title;
+};
