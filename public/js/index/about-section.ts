af9563a5df9991bb557ef33a3d1b1ec0d31db4d5
Ivo Angelov <jagdcake@tuta.io>
2019-05-18 12:01:14 +0300

    Create main about section function

--- public/js/index/about-section.ts
+++ public/js/index/about-section.ts
@@ -17,3 +17,23 @@ const showSection = (show: boolean, section: HTMLElement): boolean => {
 const replaceImage = (image: HTMLImageElement, newImageSrc: string): void => {
     image.setAttribute('src', newImageSrc);
 };
+
+const aboutSection = (): void => {
+    const toggleButton: HTMLButtonElement = document.querySelector('.about-button');
+    const buttonIcon: HTMLImageElement = toggleButton.querySelector('img');
+    const buttonOriginalIcon: string = buttonIcon.getAttribute('src');
+    const buttonCloseIcon: string = 'images/close.svg';
+
+    const section: HTMLElement = document.querySelector('.about');
+    let sectionDisplayed: boolean = false;
+
+    toggleButton.addEventListener('click', () => {
+        if (!sectionDisplayed) {
+            sectionDisplayed = showSection(true, section);
+            replaceImage(buttonIcon, buttonCloseIcon);
+        } else {
+            sectionDisplayed = showSection(false, section);
+            replaceImage(buttonIcon, buttonOriginalIcon);
+        }
+    });
+};
