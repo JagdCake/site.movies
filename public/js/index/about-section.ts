4f8e43ece985902296c3075064f25b3246713a90
Ivo Angelov <jagdcake@tuta.io>
2019-05-18 11:54:12 +0300

    Create function to show / hide the about section
    Fixes #1

---
+++ public/js/index/about-section.ts
@@ -0,0 +1,15 @@
+const showSection = (show: boolean, section: HTMLElement): boolean => {
+    if (show) {
+        section.classList.add('bg-red-pink');
+        section.classList.add('md:flex');
+        section.style.height = `${section.scrollHeight}px`;
+    } else {
+        section.style.height = '0';
+        setTimeout(() => {
+            section.classList.remove('md:flex');
+            section.classList.remove('bg-red-pink');
+        }, 400);
+    }
+
+    return show;
+};
